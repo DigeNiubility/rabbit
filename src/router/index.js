2a6c637557001f2043d5ba0ed628c19c5d424c8c
Dige <1598037040@qq.com>
2023-07-28 16:08:35 +0800

    购物车完成

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import  Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
 import Detail from '@/views/Detail/index.vue';
 import { componentSizeMap } from 'element-plus';
+import CartList from '@/views/CartList/index.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -28,7 +29,12 @@ const router = createRouter({
         {
           path:'detail/:id',
           component:Detail
+        },
+        {
+          path:'cartlist',
+          component:CartList
         }
+
       ]
 
       
--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,33 +1,71 @@
 import { defineStore } from 'pinia';
 import { ref,computed } from 'vue';
-
+import {useUserStore} from './user';
+import { insertCartAPI,findNewCartListAPI,delCartAPI } from "@/apis/cart";
 export const useCartStore =defineStore('cart',()=>{
+    const userStore=useUserStore()
+    const isLogin=computed(()=>userStore.userInfo.token)
     const cartList=ref([])
-    const addCart=(goods)=>{
-      const item=  cartList.value.find((item)=>goods.skuId===item.id)
+    const updateNewList=async()=>{
+      const res=await findNewCartListAPI()
+      cartList.value=res.result
+    }
+    const addCart=async(goods)=>{
+      const {skuId,count}=goods
+      if (isLogin.value) {
+       await insertCartAPI({skuId,count})
+       updateNewList()
+      }else{
+        const item=  cartList.value.find((item)=>goods.skuId===item.id)
         if (item) {
             item.count++
         }else{
             cartList.value.push(goods)
         }
-    
+      }
     }
-    const delCart=(skuId)=>{
-      const idx=  cartList.value.findIndex((item)=>skuId===skuId)
-      cartList.value.splice(idx,1)
+    const delCart=async(skuId)=>{
+      if (isLogin.value) {
+        await delCartAPI([skuId])
+        updateNewList()
+      }else{
+        const idx=  cartList.value.findIndex((item)=>skuId===skuId)
+        cartList.value.splice(idx,1)
+      }
+      
+    }
+    const singleCheck=(skuId,selected)=>{
+      const item=  cartList.value.find((item)=>item.skuId===skuId)
+      item.selected=selected
     }
     //计算属性
     //1.数量和总价
     
      const allCount= computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
      const allPrice= computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
-   
+    //全选
+    const isAll=computed(()=>cartList.value.every((item)=>item.selected))
+    const allCheck=(selected)=>{
+      cartList.value.forEach(item=>item.selected=selected)
+    }
+    //已选择数量和总价
+    const selectedCount=computed(()=>cartList.value.filter(item=>item.selected).reduce((a,c)=>a+c.count,0))
+    const selectedPrice=computed(()=>cartList.value.filter(item=>item.selected).reduce((a,c)=>a+c.count*c.price,0))
+    
+    
+    
+    
     return{
         cartList,
         addCart,
         delCart,
         allCount,
-        allPrice
+        allPrice,
+        singleCheck,
+        isAll,
+        allCheck,
+        selectedPrice,
+        selectedCount
 
     }
 }, {
